534d17bc864850869b545ad90e20f387c66558b0
KaistenG <129497085+KaistenG@users.noreply.github.com>
2026-04-09 14:38:52 +0200

    Main Activity with bottom navigation and empty fragments added

--- app/src/main/java/com/example/trackthat/MainActivity.java
+++ app/src/main/java/com/example/trackthat/MainActivity.java
@@ -1,24 +1,46 @@
 package com.example.trackthat;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.trackthat.ui.habits.HabitsFragment;
+import com.example.trackthat.ui.stats.StatsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        fragmentContainer = findViewById(R.id.fragmentContainer);
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigation);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_habits) {
+                showFragment(new HabitsFragment());
+                return true;
+            } else if (id == R.id.nav_stats) {
+                showFragment(new StatsFragment());
+                return true;
+            }
+            return false;
         });
+    }
+
+    private void showFragment(Fragment fragment) {
+        fragmentContainer.setVisibility(android.view.View.VISIBLE);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit();
     }
 }
